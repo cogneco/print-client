ca4549d3b60e688ff816333e1d3388307f171b1a
Jonathan Udd <jonathan.udd@vidhance.com>
2015-11-20 11:02:20 +0100

    Added etag support for PR lists

--- source/Ajax.ts
+++ source/Ajax.ts
@@ -1,11 +1,13 @@
 module PrintClient {
 	export class Ajax {
-		static loadXMLDoc(url: string, method: string, callback: any, responseType: string) {
+		static loadXMLDoc(url: string, method: string, callback: any, responseType: string, etag: string = "") {
 			var xhttp: any;
 			xhttp = new XMLHttpRequest();
 			xhttp.onload = callback;
 			xhttp.open(method, url, true);
 			xhttp.responseType = responseType;
+			if (etag)
+				xhttp.setRequestHeader("etag", etag);
 			xhttp.send();
 		}
 	}
