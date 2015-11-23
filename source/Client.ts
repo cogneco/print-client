00d34423135f6fc06c5ff416e9f16d55c532eeb5
Jonathan Udd <jonathan.udd@vidhance.com>
2015-11-23 15:33:51 +0100

    Made Client class non static

--- source/Client.ts
+++ source/Client.ts
@@ -3,62 +3,74 @@
 /// <reference path="HtmlBuilder" />
 
 module PrintClient {
-	var intervalHandle: number;
-	var pollInterval = 60000;
-	var lastEtag: string;
-	var currentPRIDs: string[] = [];
 	export class Client {
-		static loadPR(event: Event, element: Element) {
+		private intervalHandle: number;
+		private pollInterval: number;
+		private lastEtag: string;
+		private pullrequestIdList: string[] = [];		
+		constructor() {
+			this.pollInterval = 60000;
+			Ajax.loadXMLDoc("/print", "GET", this.loadReposCallback, "json");
+		}
+		loadPR(event: Event, element: Element) {
 			event.preventDefault();
 			event.stopPropagation();
 			HtmlBuilder.clearElementById("pr-container");
-			clearInterval(intervalHandle);
+			clearInterval(this.intervalHandle);
 			HtmlBuilder.appendElementById("pr-container", HtmlBuilder.createElement("h1", element.firstElementChild.textContent));
-			Ajax.loadXMLDoc(element.getAttribute("href"), "GET", Client.loadPRCallback, "json");
-			intervalHandle = setInterval(() => { Ajax.loadXMLDoc(element.getAttribute("href"), "GET", Client.loadPRCallback, "json"); }, pollInterval);
+			var returnButton = HtmlBuilder.createElement("input", "", "type", "button");
+			var href = element.getAttribute("href");
+			returnButton.setAttribute("id", "return-button");
+			returnButton.setAttribute("value", "Return to list");
+			returnButton.setAttribute("onclick", "printClient.loadRepoPRs(\"" + href.split("/")[2] + "\")");
+			document.getElementsByTagName("header")[0].children[0].appendChild(returnButton);
+			Ajax.loadXMLDoc(href, "GET", this.loadPRCallback, "json");
+			this.intervalHandle = setInterval(() => { Ajax.loadXMLDoc(href, "GET", this.loadPRCallback, "json"); }, this.pollInterval);
 		}
-		static loadRepoPRs(value: string) {
+		loadRepoPRs(value: string) {
 			PrintClient.HtmlBuilder.clearElementById("pr-container");
-			clearInterval(intervalHandle);
+			HtmlBuilder.removeElementById("return-button");
+			this.pullrequestIdList = [];
+			clearInterval(this.intervalHandle);
 			if (value != "none") {
-				HtmlBuilder.appendElementById("pr-container", HtmlBuilder.createElement("h1", value));
-				Ajax.loadXMLDoc("/print/" + value, "GET", Client.loadPRListCallback, "json");
-				intervalHandle = setInterval(() => { Ajax.loadXMLDoc("/print/" + value, "GET", Client.loadPRListCallback, "json", lastEtag); }, pollInterval);
+				/*HtmlBuilder.appendElementById("pr-container", HtmlBuilder.createElement("h1", value));*/
+				Ajax.loadXMLDoc("/print/" + value, "GET", this.loadPRListCallback, "json");
+				this.intervalHandle = setInterval(() => { Ajax.loadXMLDoc("/print/" + value, "GET", this.loadPRListCallback, "json", this.lastEtag); }, this.pollInterval);
 			}
 		}
-		static loadReposCallback(event: any) {
+		loadReposCallback(event: any) {
 			var repos = <string[]>event.target.response;
 			/*HtmlBuilder.appendElementById("repo-select", HtmlBuilder.createElement("option", "Choose repository", "value", "none"));*/
 			for (var i = 0; i < repos.length; i++) {
 				var option = HtmlBuilder.createElement("option", repos[i], "value", repos[i]);
 				if (i == 0) {
 					option.setAttribute("selected", "");
-					Client.loadRepoPRs(repos[i]);
+					printClient.loadRepoPRs(repos[i]);
 				}
 				HtmlBuilder.appendElementById("repo-select", option);					
 			}
 		}
-		static loadPRListCallback(event: any) {
+		loadPRListCallback(event: any) {
 			var pullrequests = <PrintApi.PullRequest[]>event.target.response;
 			var receivedPRs: string[] = [];
 			pullrequests.forEach((pr) => {
 				receivedPRs.push(pr.id);
 				if (!HtmlBuilder.updatePRBox(pr, true)) {
-					currentPRIDs.push(pr.id);
+					printClient.pullrequestIdList.push(pr.id);
 					HtmlBuilder.appendElementById("pr-container", HtmlBuilder.createPRBox(pr, true));
 				}
 			});
-			currentPRIDs.forEach((id) => {
+			printClient.pullrequestIdList.forEach((id) => {
 				if (receivedPRs.indexOf(id) < 0) {
 					HtmlBuilder.removeElementById(id);
-					currentPRIDs = currentPRIDs.filter((filterID) => {
+					printClient.pullrequestIdList = printClient.pullrequestIdList.filter((filterID) => {
 						return filterID != id; 
 					});
 				}
 			});
-			lastEtag = event.target.getResponseHeader("etag");
+			printClient.lastEtag = event.target.getResponseHeader("etag");
 		}
-		static loadPRCallback(event: any) {
+		loadPRCallback(event: any) {
 			var pullrequest = <PrintApi.PullRequest>event.target.response;
 			if (!HtmlBuilder.updatePRBox(pullrequest, false)) {
 				HtmlBuilder.appendElementById("pr-container", HtmlBuilder.createPRBox(pullrequest, false));
@@ -66,5 +78,4 @@ module PrintClient {
 		}
 	}
 }
-
-PrintClient.Ajax.loadXMLDoc("/print", "GET", PrintClient.Client.loadReposCallback, "json");
+var printClient = new PrintClient.Client();
