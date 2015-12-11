e276d92a0b59979a50a40ab9c1679f801b220568
Jonathan Udd <jonathan.udd@vidhance.com>
2015-12-11 10:43:54 +0100

    When loading new content the page is now reloded instead of changed with javascript

--- source/PullrequestBuilder.ts
+++ source/PullrequestBuilder.ts
@@ -30,8 +30,7 @@ module PrintClient {
 		}
 		static buildListViewPRBox(pr: PrintApi.Pullrequest) {
 			var main = HtmlBuilder.createElement("main");
-			var title = HtmlBuilder.createElement("a", "", "href", "/print/" + pr.repositoryName + "/pr/" + pr.id);
-			title.setAttribute("onclick", "printClient.loadPR(event, this)");
+			var title = HtmlBuilder.createElement("a", "", "href", "/print/print-client/" + pr.repositoryName + "/pr/" + pr.id);
 			title.appendChild(HtmlBuilder.createElement("h2", pr.title + " #" + String(pr.number)));
 			main.appendChild(title);
 			main.appendChild(PullrequestBuilder.createStatusIconForList(pr.executionResults));
@@ -47,13 +46,11 @@ module PrintClient {
 			main.appendChild(HtmlBuilder.createElement("p", "Execution status:"));
 			pr.executionResults.forEach((result) => {
 				var resultContainer = HtmlBuilder.createElement("div");
-				var taskHeader = HtmlBuilder.createElement("header", "", "onclick", "printClient.toggleOutput(event, this)"); 
+				var taskHeader = HtmlBuilder.createElement("header", "", "onclick", "printClient.toggleOutput(event, this)");
 				taskHeader.appendChild(HtmlBuilder.createElement("p", result.task, "style", "display:inline-block;margin-right:10px;"));
 				taskHeader.appendChild(PullrequestBuilder.createStatusIcon(result.result));
 				resultContainer.appendChild(taskHeader);
-				var output = HtmlBuilder.createElement("p", result.output, "class", "output");
-				output.style.display = "none";
-				resultContainer.appendChild(output);
+				resultContainer.appendChild(HtmlBuilder.createElement("details", result.output, "style", "display:none;"));
 				main.appendChild(resultContainer);
 			});
 			return main;
